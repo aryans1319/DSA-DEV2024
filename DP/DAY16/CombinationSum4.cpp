93aaeea222695c8ca8a554fe228a78de8829231e
Sudipta Ghosh <sudiptaghosh27september@gmail.com>
2022-09-10 11:02:20 +0530

    Day 16 updated c++

---
+++ DP/DAY16/CombinationSum4.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int cal(vector<int>&nums,int target, vector<int>&dp){
+        if(target==0) return 1;
+        if(target<0) return 0;
+        
+        if(dp[target]!=-1) return dp[target];
+        int count=0;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]<=target) count+=cal(nums,target-nums[i],dp);
+        }
+        return dp[target]=count;
+    }
+    
+    int combinationSum4(vector<int>& nums, int target) {
+        
+        vector<int>dp(target+1,-1);
+        return cal(nums,target,dp);
+    }
+};
